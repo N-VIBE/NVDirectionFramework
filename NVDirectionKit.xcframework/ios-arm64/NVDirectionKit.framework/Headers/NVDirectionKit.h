00cf57217987791503f09fcdab587f38f63ca9b5
dev-axel <axel@n-vibe.fr>
2023-11-20 09:44:55 +0100

    0.1.4

---
+++ NVDirectionKit.xcframework/ios-arm64/NVDirectionKit.framework/Headers/NVDirectionKit.h
@@ -0,0 +1,16 @@
+//
+//  NVDirectionKit.h
+//  NVDirectionKit
+//
+//  Created by MAC PRO N-VIBE on 07/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NVDirectionKit.
+FOUNDATION_EXPORT double NVDirectionKitVersionNumber;
+
+//! Project version string for NVDirectionKit.
+FOUNDATION_EXPORT const unsigned char NVDirectionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NVDirectionKit/PublicHeader.h>
